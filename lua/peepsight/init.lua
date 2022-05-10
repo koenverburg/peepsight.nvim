bb2c3e42db2b932937fb6be9cb1363d9760c50ee
Koen Verburg <creativekoen@gmail.com>
2022-05-10 10:19:45 +0200

    refactor: added second param for setup func to allow for custom queries

--- lua/peepsight/init.lua
+++ lua/peepsight/init.lua
@@ -6,32 +6,46 @@ local sight = require "peepsight.sight"
 local ns = vim.api.nvim_create_namespace "peepsight"
 
 local defaults = {
-  enable = true
+  enable = true,
+}
+
+local default_queries = {
+  -- go
+  "function_declaration",
+  "method_declaration",
+  "func_literal",
+
+  -- typescript
+  "arrow_function",
+  "function_declaration",
+  "generator_function_declaration",
 }
 
 function M.enable()
-  sight.sighting_in(ns)
+  sight.sighting_in(ns, M.queries)
   utils.set_autocmd()
 end
-
 
 function M.disable()
   M.options.enable = false
   view.clear(ns)
 end
 
-function M.setup(options)
+function M.setup(options, queries)
   M.options = vim.tbl_deep_extend("force", {}, defaults, options or {})
+  M.queries = vim.tbl_deep_extend("force", {}, default_queries, queries or {})
 
   if M.options.enable then
     M.enable()
   end
 end
 
+M.setup()
+
 function M.run()
   if M.options.enable then
     view.clear(ns)
-    sight.sighting_in(ns)
+    sight.sighting_in(ns, M.queries)
   end
 end
 
