346843cccd8585f9d30bdd32b90629a30d4645d3
Koen Verburg <creativekoen@gmail.com>
2022-05-29 19:00:05 +0200

    refactor: moved functions arround to lower the file count

--- lua/peepsight/utils.lua
+++ lua/peepsight/utils.lua
@@ -23,4 +23,18 @@ function utils.contains(table, element)
   return false
 end
 
+function utils.dim(namespace, buffer, line_number)
+  pcall(vim.api.nvim_buf_set_extmark, buffer, namespace, line_number, 0, {
+    end_line = line_number + 1,
+    end_col = 0,
+    hl_group = "Comment", -- mvp
+    hl_eol = true,
+    priority = 10000,
+  })
+end
+
+function utils.clear(namespace)
+  vim.api.nvim_buf_clear_namespace(0, namespace, 0, -1)
+end
+
 return utils
